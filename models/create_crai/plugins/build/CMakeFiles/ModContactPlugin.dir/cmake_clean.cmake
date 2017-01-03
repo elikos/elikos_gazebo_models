117df9afbbd1c513c8621a962d9b8f96a567cd31
Olivier St-Amour <olivierstamour0952@gmail.com>
2017-01-03 14:41:47 -0500

    Ajout des modeles qui se trouvaient dans un fork de alex

---
+++ models/create_crai/plugins/build/CMakeFiles/ModContactPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ModContactPlugin.dir/ModContactPlugin.cc.o"
+  "libModContactPlugin.pdb"
+  "libModContactPlugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ModContactPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
